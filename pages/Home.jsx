d6ba4b89fae27e51ead4009f776cb4b83a08d343
omkar-mohanty <franzohouser@gmail.com>
2022-04-09 21:52:40 +0530

    src/pages: updated UI

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,13 @@
 import { Typography } from "@mui/material";
 import { CenterGrid } from "../components/GridItem";
+import NavBar from "../components/Navbar";
 
 export default function Home(props){
     return(
       <CenterGrid>
+          <NavBar>
+              
+          </NavBar>
           <Typography variant="h1">
             TransactOne
           </Typography>
