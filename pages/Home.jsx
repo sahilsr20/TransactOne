04913a2b8f85e8cdbef14215db3e9182e168171f
omkar-mohanty <franzohouser@gmail.com>
2022-04-09 21:45:59 +0530

    src/components,src/pages: created compontnts and pages directory

---
+++ pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import { CenterGrid } from "../components/GridItem";
+
+export default function Home(props){
+    return(
+      <CenterGrid>
+          <Typography variant="h1">
+            TransactOne
+          </Typography>
+      </CenterGrid>   
+    );
+};
