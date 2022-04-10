a1f28232af1ef043addde0984f0f8f370f6480d2
omkar-mohanty <franzohouser@gmail.com>
2022-04-10 20:55:24 +0530

    test: added transaction test

--- test/transaction-test.js
+++ test/transaction-test.js
@@ -4,6 +4,7 @@ const { ethers } = require("hardhat");
 describe("Transaction", function () {
   it("Should return the correct length of current transactions", async function () {
     const Transactions = await ethers.getContractFactory("Transactions");
+    const signers = await ethers.getSigners();
     const transaction = await Transactions.deploy();
     await transaction.deployed();
 
@@ -11,7 +12,25 @@ describe("Transaction", function () {
     let currentTransactions = await transaction.getAllTransactions();
     let currentTransactionCount = await transaction.getTransactionCount();
 
+    expect(currentTransactions.length==currentTransactionCount).to.equal(true);
+  });
+
+  it("Should add transaction to blockchain",async function() {
+    const Transactions = await ethers.getContractFactory("Transactions");
+    const signers = await ethers.getSigners();
+    const transaction = await Transactions.deploy();
+    await transaction.deployed();
+
+
+    let currentTransactions = await transaction.getAllTransactions();
+    let currentTransactionCount = await transaction.getTransactionCount();
+    expect(currentTransactions.length==currentTransactionCount).to.equal(true);
+
+    await transaction.connect(signers[0]).addToBlockchain(signers[1].address,100,"Test Word","KeyWord");
+    currentTransactions = await transaction.getAllTransactions();
+    currentTransactionCount = await transaction.getTransactionCount();
 
     expect(currentTransactions.length==currentTransactionCount).to.equal(true);
+
   });
 });
